d46e486c81fa72d10b0747f2500a0619aadffeb5
Garrett Boone <boone.garrett@gmail.com>
2016-03-31 10:16:10 -0500

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@
 
         $(".scroll-down").arctic_scroll();
 
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+        $(".menu-button, .nav-cover, .nav-close").on("click", function (e) {
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
@@ -27,11 +27,11 @@
     $.fn.arctic_scroll = function (options) {
 
         var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+                elem: $(this),
+                speed: 500
+            },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
         allOptions.elem.click(function (event) {
             event.preventDefault();
@@ -43,14 +43,24 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove)}, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({scrollTop: toMove}, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top)}, allOptions.speed);
             }
         });
 
     };
+
+    $('.scroll-background article').append("<div class='terminal' style='height:400px;background-color:black'></div>")
+    $('.terminal').append('<p>$</p>')
+
+    $(".terminal").typed({
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 0
+    });
+
+
 })(jQuery);
